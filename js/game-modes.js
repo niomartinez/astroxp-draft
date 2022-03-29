9b8604cf4d3873b0f90eee87ce7ef21d42969331
Antonio Manuel Martinez <niosupetranmartinez@gmail.com>
2022-03-30 02:39:58 +0800

    Partial updates

--- js/game-modes.js
+++ js/game-modes.js
@@ -1,68 +1,71 @@
-(function ($){
-"use strict";
-    $('.game-gallery-active').slick({
-        centerMode: true,
-        centerPadding: '350px',
-        slidesToShow: 1,
-        prevArrow: '<span class="slick-prev"><i class="fas fa-caret-left"></i> previous</span>',
-        nextArrow: '<span class="slick-next">Next <i class="fas fa-caret-right"></i></span>',
-        appendArrows: ".slider-nav",
-        responsive: [
-            {
-                breakpoint: 1800,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1,
-                    centerPadding: '220px',
-                    infinite: true,
-                }
-            },
-            {
-                breakpoint: 1500,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1,
-                    centerPadding: '180px',
-                    infinite: true,
-                }
-            },
-            {
-                breakpoint: 1200,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1,
-                    centerPadding: '160px',
-                    arrows: false,
-                    infinite: true,
-                }
-            },
-            {
-                breakpoint: 992,
-                settings: {
-                    slidesToShow: 1,
-                    centerPadding: '60px',
-                    arrows: false,
-                    slidesToScroll: 1
-                }
-            },
-            {
-                breakpoint: 767,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1,
-                    centerPadding: '0px',
-                    arrows: false,
-                }
-            },
-            {
-                breakpoint: 575,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1,
-                    centerPadding: '0px',
-                    arrows: false,
-                }
-            },
-        ]
-    });
-})(jQuery); 
+(function ($) {
+  "use strict";
+
+  $(".game-gallery-active").slick({
+    centerMode: true,
+    centerPadding: "350px",
+    slidesToShow: 1,
+    prevArrow:
+      '<span class="slick-prev"><i class="fas fa-caret-left"></i> previous</span>',
+    nextArrow:
+      '<span class="slick-next">Next <i class="fas fa-caret-right"></i></span>',
+    appendArrows: ".slider-nav",
+    responsive: [
+      {
+        breakpoint: 1800,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          centerPadding: "220px",
+          infinite: true,
+        },
+      },
+      {
+        breakpoint: 1500,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          centerPadding: "180px",
+          infinite: true,
+        },
+      },
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          centerPadding: "160px",
+          arrows: false,
+          infinite: true,
+        },
+      },
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 1,
+          centerPadding: "60px",
+          arrows: false,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          centerPadding: "0px",
+          arrows: false,
+        },
+      },
+      {
+        breakpoint: 575,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          centerPadding: "0px",
+          arrows: false,
+        },
+      },
+    ],
+  });
+})(jQuery);
